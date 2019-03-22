f99b57ed39f427437070cfa263907a8a6b03d52c
FrancoUR10 <47418302+FrancoUR10@users.noreply.github.com>
2019-03-22 12:25:28 -0300

    Practica de la clase 1

--- Clase1_Programación/main.c
+++ Clase1_Programación/main.c
@@ -31,3 +31,21 @@ int main()
     system("pause");
     return 0;
 }
+
+
+/*
+Ejercicio: "La base es ingresar numeros, y resolverlo con while."
+Se ingresan numeros enteros (no se sabe cuantos). Los numeros tienen que estar comprendidos entre -500 y 500.
+Determinar:
+a) Cuantos numeros pares
+b) Cuantos positivos
+c) Cuantos negativos
+d) Cuantos ceros
+e) Promedio de numeros positivos
+f) Maximo y minimo (en que orden se ingreso) (utilizar banderas) El booleano se representa con un entero (0 o 1).
+g) La suma de los negativos
+h) La diferencia entre negativos y positivos
+i) Cuantos numeros estan entre -239 y 5
+Practicar toda la parte de estructuras repetitivas.
+Git hub : "German2017 // CodeutnFra"
+*/
