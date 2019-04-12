528ddbe1ad3ade485325e351e804402253ba8ae1
FrancoUR10 <47418302+FrancoUR10@users.noreply.github.com>
2019-04-12 10:37:07 -0300

    Clase_4_Estructuras

---
+++ Clase_4_Estructuras/main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*
+           Simples (int,float,char)
+Variables
+                                Unidimencionales
+                        Arrays
+                                Dimencionales
+           Compuestas
+                              Estructuras
+*/
+
+
+
+struct elAlumno
+{
+    char nombre[50];
+    char apellido[50];
+    int edad;
+    int nota;
+}
+
+int main()
+{
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
