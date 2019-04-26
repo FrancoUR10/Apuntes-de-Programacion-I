9d67ba72b8a24e190163cd189cf4bfffd9345e72
FrancoUR10 <47418302+FrancoUR10@users.noreply.github.com>
2019-04-26 11:59:44 -0300

    Clase_5

--- Clase_5/main.c
+++ Clase_5/main.c
@@ -2,14 +2,17 @@
 #include <stdlib.h>
 #include <string.h>
 #define TAM 3
+#define LIBRE 0
+#define OCUPADO 1
+#define BORRADO -1
 /*
 Fechas:
-viernes 26/4
-jueves 2/5 Parcial LaboratorioI ?
-viernes 3/5
-jueves 9/5
+viernes 26/4 Clase
+jueves 2/5 Clase
+viernes 3/5 Clase
+jueves 9/5 Parcial LaboratorioI(ABM)
 viernes 10/5 Parcial ProgramacionI
-jueves 16/5
+jueves 16/5 ParcialLabo(INFORMES)
 viernes 17/5 Carga notas
 */
 
